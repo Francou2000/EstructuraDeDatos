2d1bd8dcb5dde26cb225b19019ead9f20049a845
FranPisarri <franciscopisarri@gmail.com>
2024-04-06 18:09:42 -0300

    Mov corregido

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,30 +5,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
 
-    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
     [SerializeField] private float jumpForce;
 
 
-    public Vector2 MovementUpdate(float movHorizontal, bool jump, float dt, Vector2 initialPosition)
-    {
-        Vector2 deltaMovement = initialPosition;
-        deltaMovement.x = initialPosition.x + movHorizontal * speed * dt;
-        //if (jump)
-        //{
-        //    deltaMovement.y = initialPosition.y + jumpForce * dt;
-        //}
-     
-        return deltaMovement;
-    }
-
-    public float VelX(float movHorizontal)
-    {
-        float velocity = movHorizontal * speed;
-        return velocity;
-    }
-
-    public float UseJump()
-    {
-        return jumpForce;
-    }
+    public float MaxSpeed => maxSpeed;
+    public float JumpForce => jumpForce;
 }
+
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -20,12 +20,6 @@ public class Player : MonoBehaviour
     void Update()
     {
         _inputManager.InputUpdate();
-        //if (_inputManager.MovHorizontal != 0 )
-        //{
-        //    transform.position = _movement.MovementUpdate(_inputManager.MovHorizontal, _inputManager.Jump, Time.deltaTime, transform.position);
-        //}
-
-        _rb.velocity = new Vector2(_movement.VelX(_inputManager.MovHorizontal), _rb.velocity.y);
 
         //Set isOnGround
         RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(0, -1), 1, groundMask);
@@ -43,10 +37,21 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        
         if (_inputManager.Jump && isOnGround)
         {
-           _rb.AddForce(new Vector2(0, _movement.UseJump()), ForceMode2D.Impulse);
+           _rb.AddForce(new Vector2(0, _movement.JumpForce), ForceMode2D.Impulse);
         }
+
+        if (_inputManager.MovHorizontal != 0)
+        {
+            _rb.velocity = new Vector2(_movement.MaxSpeed * _inputManager.MovHorizontal, _rb.velocity.y);
+        }
+        else
+        {
+            _rb.velocity = new Vector2(0, _rb.velocity.y);
+        }
+
         
     }
 }
