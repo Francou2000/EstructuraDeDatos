0abc9192d4a60630613636347e54cb618fdbc438
Franco Urbini <francourbini@gmail.com>
2024-04-08 23:48:45 -0300

    Enemy continued

---
+++ Assets/Scripts/Enemies/EnemyMelee.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : MonoBehaviour
+{
+    public float attackCD;
+    private float CDTimer = Mathf.Infinity;
+    public float range;
+    public BoxCollider2D boxCollider;
+    public float colliderDistance;
+    
+    public int damage;
+
+    public LayerMask playerMask;
+
+    private Animator animator;
+
+    public void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        CDTimer += Time.deltaTime;
+
+        if (PlayerInSight())
+        {
+            if (CDTimer >= attackCD)
+            {
+                CDTimer = 0;
+
+                animator.SetTrigger("Attack");
+            }
+        }
+    }
+
+    private bool PlayerInSight()
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z), 0, Vector2.left, 0, playerMask);
+        return hit.collider != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(boxCollider.bounds.center + transform.right * range * transform.localScale.x * colliderDistance, 
+            new Vector3(boxCollider.bounds.size.x * range, boxCollider.bounds.size.y, boxCollider.bounds.size.z));
+    }
+}
